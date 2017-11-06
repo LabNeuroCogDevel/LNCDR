641f65e87a9533f7358f0b9c5f44267e9bfa6494
WillForan <willforan@gmail.com>
2017-11-06 12:06:01 -0500

    force to_nii df idx to numeric

--- R/to_nii.R
+++ R/to_nii.R
@@ -4,10 +4,10 @@ require(oro.nifti)
 
 #' to_nii: give a mask and a dataframe (row per voxel: i j k value), write out a nifti file
 #' @export
-#' @param examplenii: an oro.nifti object with the header (orientation, pixdim, etc) representing voxel data
-#' @param d: voxelwise dataframe: row with i j k and voxel value
-#' @param valcol: column in d to use as voxel value
-#' @param icol,jcol,kcol: columns to use for indexing
+#' @param examplenii an oro.nifti object with the header (orientation, pixdim, etc) representing voxel data
+#' @param d voxelwise dataframe: row with i j k and voxel value
+#' @param valcol column in d to use as voxel value
+#' @param icol,jcol,kcol columns to use for indexing
 #' @examples
 #' \dontrun{
 #'  mask.file <- '/Volumes/Phillips/mMR_PETDA/scripts_BTC/HarOx-sub-2mm.striatumplusthalamus.nii.gz'
@@ -17,20 +17,27 @@ require(oro.nifti)
 #' }
 
 to_nii <- function(examplenii,d,fileout,valcol=4,icol='i',jcol='j',kcol='k') {
+ # read in data if given a file name (otherwise expect oronifti object)
  if(class(examplenii) == "character" )  {
      examplenii <- readNIfTI(examplenii) 
  }
 
+ # create a zero value matrix to populate with dataframe values
  #md <- examplenii@.Data
  #md[,,] <- 0
  dm <- dim(examplenii@.Data)
  md <- rep(0,prod(dm))
  dim(md) <- dm
 
+ # indexes should all be numeric
+ for ( coln in c(icol,jcol,kcol)){ d[,coln] <- as.numeric(d[,coln]) }
+
+ # put the value in the dataframe into the nifti matrix
  for(idx in 1:nrow(d) ){
    md[ d[idx,icol],d[idx,jcol],d[idx,kcol] ] <- d[idx,valcol]
  }
 
+ # save it
  examplenii@.Data <- md
  writeNIfTI(examplenii,fileout)
 }
--- tests/testthat/test_to_nii.R
+++ tests/testthat/test_to_nii.R
@@ -18,3 +18,19 @@ test_that("read write save", {
    file.remove('test.nii.gz')
   
 })
+
+test_that("read write save with charater indexies", {
+   d<-data.frame(i=20:30,j=20:30,k=20:30,v=100)
+   d$i <- as.character(d$i); d$k <- as.character(d$k); d$j <- as.character(d$j) 
+   to_nii('../HarOx-sub-2mm.striatumplusthalamus.nii.gz',d,'test')
+   expect_true( file.exists('test.nii.gz') )
+
+   nii <- readNIfTI('test.nii.gz')
+   nd <- nii@.Data[nii@.Data>0]
+
+   expect_length(nd, 11 )
+   expect_true( all(nd==100) )
+
+   file.remove('test.nii.gz')
+  
+})
