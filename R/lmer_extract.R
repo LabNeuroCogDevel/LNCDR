25c7cdc10646139c5638f4dcc4f2b03fe8b0fb23
WillForan <willforan@gmail.com>
2018-02-14 15:02:15 -0500

    include lmer_extract warning if bad factor name

--- R/lmer_extract.R
+++ R/lmer_extract.R
@@ -6,11 +6,12 @@
 #'  get (t, chisq, p) values from a single variable in a model
 #' @param model is a lm model
 #' @param varname is the variable of interest (string)
-#' @param factorname is optional factor of var
+#' @param factorname is optional factor of var (string) as named by summary(model), e.g. 'Female01female'
 #' @export
 #' @examples
 #'   m <- lme4::lmer(uptake ~ conc + Type + (1 | Treatment), CO2)
 #'   lmer_extract(m, "conc")
+#'   lmer_extract(m, "Type", "TypeMississippi")
 
 lmer_extract <- function(model, varname, factorname=NULL){
   if (is.null(factorname)) factorname <- varname
@@ -18,6 +19,14 @@ lmer_extract <- function(model, varname, factorname=NULL){
   m_s <- summary(model)
   tall <- m_s$coefficients[, 3]
   t_at_name <- tall[names(tall) == factorname]
+
+  # warn if t_at_name does not exist
+  if (length(t_at_name) == 0L) {
+     warning(
+      sprintf('no tval returned! Factor name "%s" not in model output: %s',
+         factorname,
+         paste(sep=", ", collapse=", ", names(tall))))
+  }
 
   m_ca <- as.data.frame(car::Anova(model))
   cs   <- m_ca$Chisq[row.names(m_ca) == varname]
