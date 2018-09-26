74db3d413869f39c0c26a5696f4bfaafe1a13205
WillForan <willforan@gmail.com>
2018-09-26 15:36:06 -0400

    more options for date_match

--- R/date_match.R
+++ R/date_match.R
@@ -2,12 +2,15 @@
 # Author:  Will Foran
 # 
 #' match dates in a data frame by one-to-many merge and rank. Useful to match behavioral to brain over multiple timepoints.
-#' @param d1 is the main dataframe 
-#' @param d2 is the supplimental dataframe with dates to match
-#' @param datecol1 is date column one, this will be the id column
-#' @param datecal2 is date column two. this will be the many in one-to-many merge. defaults to same name as datacol1
-#' @param maxdatediff is the maximum time between matches (default 180 days)
-#' @param maxrank is highest index value of datediff ranked. set to 1 to ingore ties (deafult is 1.999)
+#' @param d1 -- main dataframe (has the "true" date. one part of one-to-many)
+#' @param d2 -- supplimental dataframe with dates to match (many part of one-to-many)
+#' @param id -- column in both d1 and d2 (lunaid)
+#' @param datecol1 -- date column one, this will be the id column
+#' @param datecal2 -- date column two. this will be the many in one-to-many merge. defaults to same name as datacol1
+#' @param all.x -- weather or not to keep NAs (b/c match is over maxdatediff or does not existed)
+#' @param suffix.y -- what to call the d2 columns that match d1 names
+#' @param maxdatediff -- maximum time between matches (default 180 days)
+#' @param maxrank -- highest index value of datediff ranked. set to 1 to ingore ties (deafult is 1.999)
 #' @export
 #' @examples 
 #'   dummy1 <- data.frame(id=c(1,2,3,3),d=c(20120302,20120302,20120302,20150302),j=1)
@@ -18,43 +21,71 @@
 #' 
 #' 
 
-date_match <-function(d1,d2,idcol,datecol1,datecol2=datecol1,maxdatediff=180,maxrank=2-1e-3){
+date_match <-function(d1, d2, idcol, datecol1, datecol2=datecol1, all.x=F,
+                      suffix.y=".y", maxdatediff=180, maxrank=2-1e-3){
    require(dplyr)
 
    # change name if datecolumns are the same
-   if(datecol1==datecol2) {
-      datecol2=paste0(datecol1,'.y')
+   # changing now saves us from having .x and .y
+   if (datecol1==datecol2) {
+      datecol2 <- paste0(datecol1, ".y")
       names(d2)[ which(names(d2)==datecol1) ] <- datecol2
    }
 
-   # check data time. we dont _need_ columns to be Date type, but it'll probably be an issue
-   if(class(d1[1,datecol1]) != 'Date' ||
-      class(d2[1,datecol2]) != 'Date') { 
-        warning('not all specified date columns are "Date" type. maxdatediff will be weird.\n\t** consider e.g. d$datecol <- lubridate::ymd(datecol)  **')
+   # check data time.
+   # we dont _need_ columns to be Date type, but it'll probably be an issue
+   ctypes <- c(class(d1[1, datecol1]), class(d2[1, datecol2]))
+   if (!all(grepl("Date", ctypes))) {
+        warning('not all specified date columns are "Date" type: ',
+                paste(collapse=" ", ctypes),
+                ". maxdatediff will be weird.\n",
+                "\t** consider e.g. d$datecol <- lubridate::ymd(datecol)  **")
    }
 
    # merge all the d2 onto each potential d1 match
-   diffstr <- sprintf("%s-%s",datecol1,datecol2)
-   onetomany <- 
-      merge(d1,d2,by=idcol) %>%
-      group_by_(idcol,datecol1) %>%
+   diffstr <- sprintf("%s-%s", datecol1, datecol2)
+   onetomany <-
+      merge(d1, d2, by=idcol, all.x=all.x, suffixes=c("", suffix.y)) %>%
+      group_by_(idcol, datecol1) %>%
       mutate_(datediff=diffstr)  %>%
       mutate(r= datediff %>% abs %>% rank )
-   
+
    # todo: report ties with warning??
 
-   returnval <- onetomany %>% filter(r<=maxrank, abs(datediff) <= maxdatediff) %>% select(-r) %>% ungroup
+   # NA out one that are too high
+   row_na <- which(abs(onetomany$datediff) > maxdatediff)
+   col_na <- which(! names(onetomany) %in% names(d1) &
+               ! names(onetomany) %in% c(idcol, datecol2, "r", "datediff")) %>%
+             unique
+   for (r in row_na) onetomany [r, col_na] <- NA
+
+   # remove where datediff is too big
+   if (!all.x && length(row_na)>0L) onetomany <- onetomany[-row_na, ]
+
+   returnval <- onetomany %>%
+      filter(r<=maxrank) %>%
+      select(-r) %>%
+      ungroup
+
+   # if we are going to keep datediff around,
+   # make sure we know where it came from
+   names(returnval)[names(returnval) == "datediff"] <-
+      paste0("datediff", suffix.y)
+
 
    # check repeated date2 (best match is not unique)
-   d2check <- returnval[,c(idcol,datecol2)]
+   d2check <- returnval[, c(idcol, datecol2)]
    d2reps   <- duplicated(d2check)
-   if(any(d2reps)) {
-      dispreps <- merge( returnval[,c(idcol,datecol1,datecol2) ], d2check[d2reps,], by=c(idcol,datecol2) )
-      dfstr <- capture.output( dispreps %>% print.data.frame(row.names=F) ) %>%  paste(collapse="\n")
-      warning( 'best match is not unique: repeated second date for subject+first date\n', dfstr, '\n')
+   if (any(d2reps)) {
+      dispreps <- merge( returnval[, c(idcol, datecol1, datecol2) ],
+                         d2check[d2reps, ],
+                         by=c(idcol, datecol2) )
+      dfstr <- capture.output( dispreps %>% print.data.frame(row.names=F) ) %>%
+         paste(collapse="\n")
+      warning("best match is not unique: ",
+              "repeated second date for subject+first date\n",
+              dfstr, "\n")
    }
 
    return(returnval)
 }
-
-
---
+++ tests/testthat/test_date_match.R
@@ -0,0 +1,58 @@
+library(LNCDR)
+context("date_match")
+# test with
+# cd ../; Rscript testthat.R
+
+# example data
+d.o<-data.frame(id=c(1, 2, 2, 3),
+                thing=8:11,
+                ymd=lubridate::ymd(c("20180910", "20171011",
+                                    "20161213", "20150102")))
+dm<-data.frame(id=c(1, 1, 1, 1),
+               otherfield=1:4,
+               ymd=lubridate::ymd(c("20180926", "20171010",
+                                    "20000102", "20190905")))
+dm2<-data.frame(id=c(1, 1, 2, 2),
+                otherfield=1:4,
+                ymd=lubridate::ymd(c("20180926", "20171010",
+                                    "20000102", "20190905")))
+
+test_that("remove missing", {
+ d <- date_match(d.o, dm, "id", "ymd")
+ expect_equal(nrow(d), 1)
+ expect_equal(d$id, 1)
+ expect_equal(d$ymd.y, lubridate::ymd("20180926"))
+ expect_true(all(c("thing", "otherfield") %in% names(d)))
+})
+
+test_that("keep missing", {
+ d <- date_match(d.o, dm, "id", "ymd", all.x=T)
+ expect_equal(nrow(d), 4)
+ expect_true(d$ymd.y[d$id==1] == lubridate::ymd("20180926"))
+ expect_true(is.na(d$ymd.y[d$id==3]))
+})
+
+test_that("same matching date within max", {
+ d <- date_match(d.o, dm2, "id", "ymd")
+ expect_equal(nrow(d), 1)
+ # both id=2 are out of max range
+})
+
+test_that("same matching date no max", {
+ d <- date_match(d.o, dm2, "id", "ymd", maxdatediff=Inf, all.x=T)
+ expect_equal(nrow(d), 4)
+ expect_true(is.na(d$ymd.y[d$id==3]))
+ # both id==2 will have the same match
+})
+
+test_that("same matching date, nanmax ", {
+ d <- date_match(d.o, dm2, "id", "ymd", all.x=T)
+ # have all 4 ids (we ketp all)
+ expect_equal(nrow(d), 4)
+ # all of 2 are too old so we NA otherfield
+ expect_true(all(is.na(d$otherfield[d$id==2])))
+ # but keep date and datediff
+ expect_true(all(!is.na(d$ymd.y[d$id==2])))
+})
+
+# tooo: only one dm date matching 2 do dates
