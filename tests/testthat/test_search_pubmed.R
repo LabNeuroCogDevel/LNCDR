eada1fe1413e9ae9fb6025ae20a997a4cf9a8e1c
WillForan <willforan@gmail.com>
2018-11-09 14:34:21 -0500

    pubmed search: update to test reading in xml too

--- tests/testthat/test_search_pubmed.R
+++ tests/testthat/test_search_pubmed.R
@@ -1,14 +1,29 @@
 library(LNCDR)
 context("pubmed_search")
 
+# run this test twice -- once to download, another to reuse
+btc <- function(){
+    d <- pubmed_search("Tervo-Clemmens[Author]", "temp/authsearch")
+    # have more than one result
+    expect_true(nrow(d) > 4)
+    # have expected columns
+    expect_true(all(names(d) %in% c("journal", "title", "year",
+                                    "abstract", "doi", "authors" )))
+    # have one of the dois
+    expect_true("10.1016/j.bpsc.2018.05.004" %in% d$doi)
+}
 
-test_that("same matching date, nanmax ", {
- d <- pubmed_search("Tervo-Clemmens[Author]", "temp/authsearch")
- # have more than one result
- expect_true(nrow(d) > 4)
- # have expected columns
- expect_true(all(names(d) %in% c("journal", "title", "year",
-                                 "abstract", "doi", "authors" )))
- # have one of the dois
- expect_true("10.1016/j.bpsc.2018.05.004" %in% d$doi)
+test_that("download results", {
+    expect_true(length(Sys.glob("temp/authsearch*xml")) == 0)
+    btc()
+})
+test_that("reuse results", {
+    expect_true(length(Sys.glob("temp/authsearch*xml")) > 0)
+    btc()
+})
+
+# remove temporary files when we finish
+teardown({
+    lapply(Sys.glob("temp/authsearch*xml"),unlink)
+    unlink("temp")
 })
