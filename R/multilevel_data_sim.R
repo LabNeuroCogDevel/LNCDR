e3cc801a13645820808c2dbf91054f428d9c13c2
WillForan <willforan@gmail.com>
2019-05-21 11:03:35 -0400

    add docstrings, reduce args in retentionadjustvisits

--- R/multilevel_data_sim.R
+++ R/multilevel_data_sim.R
@@ -1,26 +1,24 @@
+# Author:  Brenden Tervo-Clemmens
+# Packager: Will Foran
+
+# cal error: used by either simulation method
 errvar_ICCandrandomeffectvar <- function(ICC, randvar = 1) {
     errorvar <- (randvar - (ICC * randvar))/ICC
     return(data.frame(variance = errorvar, sd = sqrt(errorvar)))
 }
 
-retentionadjustvisits <-
-   function(nsubject, nvisits, retentionrate, totalvisits = NULL) {
-    if (is.null(totalvisits)) {
-        retentionadjust <- retentionrate
-        adjustedtotalvisits <- round(retentionadjust * (nsubject * nvisits),
-                                     digits = 0)
-        print(sprintf("%s total visits based on %s rention rate",
-                      adjustedtotalvisits, retention))
-    } else {
-        print(sprintf("%s total visits requested", totalvisits))
-        retentionadjust <- totalvisits/ (nsubject * nvisits)
-        print(sprintf("calculated retention rate is %s", retentionadjust))
-        adjustedtotalvisits <- round(retentionadjust * (nsubject * nvisits),
-                                     digits = 0)
-    }
-    return(c(retentionadjust, adjustedtotalvisits))
-}
 
+#' sim_multilevel_bivariatenormal_randintercept 
+#'
+#' @description simulate multilevel bivariate normal dist with a random intercept
+#' @param  nsubject        how many subjects
+#' @param  nvisits         how many visits each
+#' @param  interceptcor    intercept correlation
+#' @param  ICCs            inter class correlation
+#' @importFrom MASS mvrnorm
+#' @export
+#' @examples
+#'   sim_multilevel_bivariatenormal_randintercept(100,2,.3,.4)
 sim_multilevel_bivariatenormal_randintercept <-
    function(nsubject, nvisits, interceptcor, ICCs) {
     n <- nsubject
@@ -29,10 +27,9 @@ sim_multilevel_bivariatenormal_randintercept <-
     subject <- rep(1:n, each = timepoints)
 
     rand_eff_corr <- matrix(c(1, interceptcor, interceptcor, 1), ncol = 2)
-    random_effects <- MASS::mvrnorm(n, mu = c(0, 0),
-                                    Sigma = rand_eff_corr,
-                                    empirical = FALSE) %>%
-        data.frame()
+    random_effects <- data.frame(MASS::mvrnorm(n, mu = c(0, 0),
+                                               Sigma = rand_eff_corr,
+                                               empirical = FALSE))
     colnames(random_effects) <- c("Int1", "Int2")
 
     # set fixed effects to mu zero for sim
@@ -59,6 +56,22 @@ sim_multilevel_bivariatenormal_randintercept <-
     return(d)
 }
 
+
+
+#' sim_multilevel_bivariatenormal_randinterceptandslope
+#'
+#' @description simulate multilevel bivariate normal dist with a random intercept AND SLOPE
+#' @param  nsubject           how many subjects
+#' @param  nvisits            how many visits each
+#' @param  interceptcor       intercept correlation
+#' @param  slopecor           slope correlation
+#' @param  slopeintcorbetween slope between subject correlation
+#' @param  slopeintcorwithin  slope within subject correlation
+#' @param  ICCs               inter class correlation
+#' @importFrom Mass mvrnorm
+#' @export
+#' @examples
+#'  sim_multilevel_bivariatenormal_randinterceptandslope(100,2,.1,.5,.6,.3,.4)
 sim_multilevel_bivariatenormal_randinterceptandslope <-
    function(nsubject, nvisits, interceptcor, slopecor,
             slopeintcorbetween, slopeintcorwithin, ICCs) {
@@ -74,9 +87,9 @@ sim_multilevel_bivariatenormal_randinterceptandslope <-
                 slopeintcorbetween, interceptcor, slopeintcorwithin, 1),
               ncol = 4)
 
-    random_effects <- MASS::mvrnorm(n, mu = c(0, 0, 0, 0),
-                                   Sigma = rand_eff_corr, empirical = FALSE) %>%
-                     data.frame()
+    random_effects <- data.frame(MASS::mvrnorm(n, mu = c(0, 0, 0, 0),
+                                               Sigma = rand_eff_corr,
+                                               empirical = FALSE))
     colnames(random_effects) <- c("Slope1", "Int1", "Slope2", "Int2")
 
     # set fixed effects to mu zero for sim
@@ -105,3 +118,30 @@ sim_multilevel_bivariatenormal_randinterceptandslope <-
     d <- data.frame(subject, time, outcome1, outcome2)
     return(d)
 }
+
+#' retentionadjustvisits
+#'
+#' @description simulate multilevel bivariate normal dist with a random intercept
+#' @param  nsubject             how many subjects
+#' @param  nvisits              how many visits each
+#' @param  retention_or_total   ratio of retained (0<x<1) or total (x>=1)
+#' @export
+#' @examples
+#' retentionadjustvisits(100,2,.8)  # using retention
+#' retentionadjustvisits(100,2,160) # using total
+retentionadjustvisits <- function(nsubject, nvisits, retention_or_total) {
+    if (retention_or_total < 1) {
+        retentionadjust <- rate_or_total
+        print(sprintf("%s total visits based on %s retention rate",
+                      adjustedtotalvisits, retention))
+    } else {
+        totalvisits <- rate_or_total
+        print(sprintf("%s total visits requested", totalvisits))
+        retentionadjust <- totalvisits/ (nsubject * nvisits)
+        print(sprintf("calculated retention rate is %s", retentionadjust))
+    }
+    adjustedtotalvisits <- round(retentionadjust * (nsubject * nvisits),
+                                 digits = 0)
+    return(c(retentionadjust, adjustedtotalvisits))
+}
+
