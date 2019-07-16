2ddb6d9e4dcdc4494df7dc3ecf6cb045f8243793
WillForan <willforan@gmail.com>
2019-07-16 13:41:11 -0400

    add waterfall_group to generate data for waterfall plot

--- R/waterfall_plot.R
+++ R/waterfall_plot.R
@@ -1,3 +1,27 @@
+#' waterfall plot data
+#'
+#' @description create age_id ranked id for data grouped by start age
+#' @param d   dataframe with columns id (repeated) and  age
+#' @import dplyr
+#' @import ggplot2
+#' @export
+#' @examples
+#'  d <- data.frame(age=c(10,20,25, 13,14), id=c(100,100,100, 200,200))
+#'  d_grouped <- waterfall_group(d)
+waterfall_group <- function(d) {
+   if (!all(c("id", "age") %in% names(d))){
+      stop("dataframe must have columns 'id' and 'age'")
+   }
+   age_ranked <-
+      d %>%
+      select(id, age) %>%
+      group_by(id) %>%
+      summarise(minage=min(age)) %>%
+      ungroup() %>%
+      mutate(age_id = rank(minage, ties.method="first") ) %>%
+      inner_join(d, by="id")
+}
+
 #' waterfall plot
 #'
 #' @description subj x age plot: line-per-subject dot-per-age ordered by age at first visit
@@ -9,23 +33,13 @@
 #'  d <- data.frame(age=c(10,20,25, 13,14), id=c(100,100,100, 200,200))
 #'  p <- waterfall_plot(d)
 #'  print(p)
-waterfall_plot <- function(d) {
-   if (!all(c("id", "age") %in% names(d))){
-      stop("dataframe must have columns 'id' and 'age'")
-   }
+waterfall_plot <- function(d, ...) {
 
-   age_ranked <-
-      d %>%
-      select(id, age) %>%
-      group_by(id) %>%
-      summarise(minage=min(age)) %>%
-      ungroup() %>%
-      mutate(age_id = rank(minage) ) %>%
-      inner_join(d, by="id")
+   age_ranged <- waterfall_data(d)
 
    p <-
       ggplot(age_ranked) +
-      aes(x=age, y=age_id, group=age_id) +
+      aes(x=age, y=age_id, group=age_id, ...) +
       geom_line() +
       geom_point()
 
