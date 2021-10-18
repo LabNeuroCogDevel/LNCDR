06dba37f4d72ee3c4e32f49afcf9eeb35397652f
WillForan <willforan@gmail.com>
2021-10-18 12:00:43 -0400

    winsorize: update docs

--- R/winsorize_sd.R
+++ R/winsorize_sd.R
@@ -1,11 +1,15 @@
+# Author: Orma Ravindranath 
+# Packager: Will Foran
+
 #' winsozrize_sd - set above/below devation range to range max/min
+
 #' @param x vector to be winsorized
 #' @param devno number of deviations to winsorize
 #' @param dev "sd" (standard deviation) or "mad" (median absolute deviation)
-#' @example
+#' dev depending on which type of deviation should be used to winsorize
+#' @examples
 #' w <- winsorize_sd(c(1:10), 1, "sd")
 #' @export
-#' dev depending on which type of deviation should be used to winsorize
 winsorize_sd <- function(x, devno, dev="sd") {
   # TODO: dev could be givne as a function instead
   if (dev=="sd")  devrange <- sd(x, na.rm=TRUE) * devno
