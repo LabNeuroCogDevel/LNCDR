024ce04b69806a8eefb9c40ac7c78d991a6bc629
Will Foran <willforan@gmail.com>
2022-08-10 10:39:46 -0400

    ✅ db test only if psql succeeds
    testing dependency on perl URI::Encode is problematic
    consider
    stringi::URLencode(password, reserved = TRUE)

--- tests/testthat/test_dbquery.R
+++ tests/testthat/test_dbquery.R
@@ -3,10 +3,15 @@ context("db_query")
 # test with
 # cd ../; Rscript testthat.R
 
+dbipath <- function()
+    system("perl -MURI::Encode=uri_encode -slanF: -e 'if(/lncddb/){print \"postgres://$F[3]:\",uri_encode($F[4], { encode_reserved => 1 }), \"\\@$F[0]:$F[1]/$F[2]\"; exit}' ~/.pgpass",intern=TRUE)
+db_exists <- function()
+   file.exists("~/.pgpass") &&
+   any(grepl("lncddb", readLines("~/.pgpass"))) &&
+    system(paste0("psql ", dbipath(), " -c 'select pid from enroll limit 1'"), timeout=5) == 0
 
 test_that("can query", {
- skip_if_not(file.exists("~/.pgpass") &&
-             any(grepl("lncddb", readLines("~/.pgpass"))))
+ skip_if_not(db_exists())
  d <- db_query("select study from study")
  expect_true("CogR01" %in% d$study)
 })
@@ -14,8 +19,7 @@ test_that("can query", {
 test_that("close connection if not provided", {
  # previously if not closed got error
  # RS-DBI driver: (@cannot allocate a new connection -- maximum of 16 connections ...
- skip_if_not(file.exists("~/.pgpass") &&
-             any(grepl("lncddb", readLines("~/.pgpass"))))
+ skip_if_not(db_exists())
  for (i in 1:100) {
     d <- db_query("select study from study")
     expect_true("CogR01" %in% d$study)
